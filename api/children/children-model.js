51effef071d22cad97aca47618b459fad048bae9
Travis Little <kingtrav@gmail.com>
2019-09-19 16:27:03 -0600

    basic get and post child routes created

--- api/children/children-model.js
+++ api/children/children-model.js
@@ -22,6 +22,6 @@ function findById(id) {
 function add(child) {
     return db('children').insert(child)
         .then(ids => {
-            return findById(ids[0])
+            return findById(ids[0]) // returns the new child object
         })
 }
--- api/server.js
+++ api/server.js
@@ -1,14 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+
+// MIDDLEWARE
 const helmet = require('helmet')
 const cors = require('cors')
 
+// ROUTES
+const childrenRouter = require('./children/children-router.js')
 
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
 server.use(cors())
+
+// ROUTES
+server.use('/api/children', childrenRouter)
 
 server.get('/', (req, res) => {
     res.send(`Immunization Tracker Back-End API BW Unit 4 - Travis Little WebPT6`)
