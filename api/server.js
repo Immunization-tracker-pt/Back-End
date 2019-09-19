869e707ed6badfc7a6ced13d4d559e0a21bee6ca
Travis Little <kingtrav@gmail.com>
2019-09-19 17:21:11 -0600

    immunization router get and post created

--- api/server.js
+++ api/server.js
@@ -8,7 +8,8 @@ const cors = require('cors')
 // ROUTES
 const childrenRouter = require('./children/children-router.js')
 const parentsRouter = require('./parents/parents-router.js')
-const doctorsRouter = require('./doctors/doctors-router')
+const doctorsRouter = require('./doctors/doctors-router.js')
+const immunizationsRouter = require('./immunizations/immunizations-router.js')
 
 const server = express()
 
@@ -20,6 +21,7 @@ server.use(cors())
 server.use('/api/children', childrenRouter)
 server.use('/api/parents', parentsRouter)
 server.use('/api/doctors', doctorsRouter)
+server.use('/api/immunizations', immunizationsRouter)
 
 server.get('/', (req, res) => {
     res.send(`Immunization Tracker Back-End API BW Unit 4 - Travis Little WebPT6`)
