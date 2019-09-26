6b06a01e553fbcea0fafc5b549b45afad7fe9f76
Travis Little <kingtrav@gmail.com>
2019-09-25 20:37:05 -0600

    added doctor end points to request permission

--- api/doctors/doctors-model.js
+++ api/doctors/doctors-model.js
@@ -4,7 +4,13 @@ module.exports = {
     find,
     findBy,
     findById,
-    add
+    add,
+    getParentsWithDoctorId,
+    getImmunizationsByDoctorId,
+    getImmunizationsByChildId,
+    getImmunizationsByParentId,
+    requestPermission,
+    revokePermissionRequest
 }
 
 function find() {
@@ -24,4 +30,44 @@ function add(doctor) {
         .then(ids => {
             return findById(ids[0])
         })
+}
+
+function getParentsWithDoctorId(doctor_id) {
+    return db('parent_doctor_detail as pdd')
+        .join('parents as p', 'pdd.parent_id', '=', 'p.id')
+        .select('p.id as parent_id', 'p.firstname', 'p.lastname', 'p.email', 'p.dob', 'p.street', 'p.city', 'p.state_province', 'p.phonenumber', 'p.permission_granted as PARENT_permission_granted', 'pdd.permission_granted', 'pdd.permission_requested')
+        .where('pdd.doctor_id', doctor_id)
+}
+
+function requestPermission(parent_id, doctor_id) {
+    return db('parent_doctor_detail')
+        .where({parent_id, doctor_id})
+        .update({
+            permission_requested: 1
+        })
+        .then(() => {
+            return db('parent_doctor_detail').where({parent_id, doctor_id})
+        })
+}
+
+function revokePermissionRequest(parent_id, doctor_id) {
+    return db('parent_doctor_detail')
+        .where({parent_id, doctor_id})
+        .update({
+            permission_requested: 0
+        })
+        .then(() => {
+            return db('parent_doctor_detail').where({parent_id, doctor_id})
+        })
+}
+function getImmunizationsByDoctorId(doctor_id) {
+    return db('immunizations').where({doctor_id})
+}
+
+function getImmunizationsByParentId(parent_id) {
+    return db('immunizations').where({parent_id})
+}
+
+function getImmunizationsByChildId(child_id) {
+    return db('immunizations').where({child_id})
 }
--- api/immunizations/immunizations-model.js
+++ api/immunizations/immunizations-model.js
@@ -41,11 +41,9 @@ function addInitialImmunizations(parent_id, child_id) {
             child_id, parent_id, name: immunization 
         })
     })
-    // console.log("CHECK THIS", init_immunizations)
 
     return db('immunizations').insert(init_immunizations)
         .then(data => {
-            // console.log("does this happen", data)
             return findByChildId(child_id)
             
         })
